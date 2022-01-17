f7a04f7ef08921de024e9d0c8e3f194c394717a6
ZillowwCode <raphaelbrancovieira.pro@gmail.com>
2022-01-18 00:31:29 +0100

    Creation and implementation of the Header

--- Version Web/client/src/App.js
+++ Version Web/client/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Typography, Container, Divider } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
-function App() {
+const useStyles = makeStyles({
+  title: {
+    color: "#fff",
+    textShadow: "0 0 5px #000",
+  },
+
+  subTitle: {
+    color: "rgba(0, 0, 0, 0.5)",
+  },
+
+  divider: {},
+});
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <div id="header">
+        <Typography
+          sx={{ fontSize: 144, marginTop: "100px" }}
+          className={classes.title}
+          variant="h1"
+          align="center"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          FilmMatch
+        </Typography>
+
+        <Typography
+          className={classes.subTitle}
+          sx={{ fontSize: 64 }}
+          variant="h2"
+          align="center"
+        >
+          Compliqué de trouver un film à deux ? <br />
+          Nous avons la solution.
+        </Typography>
+
+        <Divider
+          sx={{ marginTop: "40px", width: "950px" }}
+          className={classes.divider}
+        />
+      </div>
+    </Container>
   );
-}
+};
 
 export default App;
