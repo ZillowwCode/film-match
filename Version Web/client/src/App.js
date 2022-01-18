e10bb17f7ab6ee8ec730c690cee1526ef2935584
ZillowwCode <raphaelbrancovieira.pro@gmail.com>
2022-01-18 14:31:53 +0100

    Adding Dummy Films and automation of Film List display

--- Version Web/client/src/App.js
+++ Version Web/client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography, Container, Divider, Grid } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
@@ -21,10 +21,40 @@ const useStyles = makeStyles({
 
 const App = () => {
   const classes = useStyles();
+  const [films, setFilms] = useState([
+    {
+      title: "Les Éternels",
+      synopsis:
+        "Depuis l’aube de l’humanité, les Éternels, un groupe de héros venus des confins de l’univers, protègent la Terre. Lorsque les Déviants, des créatures monstrueuses que l’on croyait disparues depuis longtemps, réapparaissent mystérieusement, les Éternels sont à nouveau obligés de se réunir pour défendre l’humanité…",
+      note: 7.2,
+      imgUrl:
+        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vV7TrS7PNRZJHCxNmiYN1SU7s1w.jpg",
+      date: "3 nov. 2021",
+    },
+
+    {
+      title: "Spider-Man: No Way Home",
+      synopsis:
+        "Après les évènements liés à l'affrontement avec Mystério, l'identité secrète de Spider-Man a été révélée. Il est poursuivi par le gouvernement américain, qui l'accuse du meurtre de Mystério, et est traqué par les médias. Cet évènement a également des conséquences terribles sur la vie de sa petite-amie M. J. et de son meilleur ami Ned. Désemparé, Peter Parker demande alors de l'aide au Docteur Strange. Ce dernier lance un sort pour que tout le monde oublie que Peter est Spider-Man. Mais les choses ne se passent pas comme prévu et cette action altère la stabilité de l'espace-temps. Cela ouvre le « Multivers », un concept terrifiant dont ils ne savent quasiment rien.",
+      note: 8.4,
+      imgUrl:
+        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/3SyG7dq2q0ollxJ4pSsrqcfRmVj.jpg",
+      date: "15 déc. 2021",
+    },
+
+    {
+      title: "S.O.S. Fantômes : L’Héritage",
+      synopsis:
+        "Une mère célibataire et ses deux enfants s'installent dans une petite ville et découvrent peu à peu leur relation avec les chasseurs de fantômes et l'héritage légué par leur grand-père.",
+      note: 7.7,
+      imgUrl:
+        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/w1cmavTc0IwISr9ypI1XkBtxhVK.jpg",
+      date: "11 nov. 2021",
+    },
+  ]);
 
   const handleFormSubmit = (film1, film2) => {
-    alert(film1);
-    alert(film2);
+    alert("Films recus !");
   };
 
   return (
@@ -61,34 +91,20 @@ const App = () => {
         </div>
 
         <div id="filmsList">
-          <Grid
-            container
-            spacing={3}
-            sx={{ marginTop: "50px", marginBottom: "220px" }}
-          >
-            <Grid item xs={12} sm={6} md={4}>
-              <FilmItem />
-            </Grid>
-
-            <Grid item xs={12} sm={6} md={4}>
-              <FilmItem />
-            </Grid>
-
-            <Grid item xs={12} sm={6} md={4}>
-              <FilmItem />
-            </Grid>
-
-            <Grid item xs={12} sm={6} md={4}>
-              <FilmItem />
-            </Grid>
-
-            <Grid item xs={12} sm={6} md={4}>
-              <FilmItem />
-            </Grid>
-
-            <Grid item xs={12} sm={6} md={4}>
-              <FilmItem />
-            </Grid>
+          <Grid container sx={{ marginTop: "50px", marginBottom: "220px" }}>
+            {films.map((film) => {
+              return (
+                <Grid xs={12} sm={6} md={4}>
+                  <FilmItem
+                    title={film.title}
+                    img={film.imgUrl}
+                    synopsis={film.synopsis}
+                    date={film.date}
+                    note={film.note}
+                  />
+                </Grid>
+              );
+            })}
           </Grid>
         </div>
       </Container>
--- Version Web/client/src/Components/FilmItem.jsx
+++ Version Web/client/src/Components/FilmItem.jsx
@@ -9,27 +9,23 @@ import {
 
 const FilmItem = (props) => {
   return (
-    <Card elevation={10}>
+    <Card elevation={3} sx={{ margin: "10px" }}>
       <CardHeader
         sx={{
           backgroundColor: "#262730",
           color: "#F6F7EB",
           textAlign: "center",
         }}
-        title="Titre du film"
+        title={props.title}
+        subheader={
+          <Typography variant="body2">
+            [{props.date}] - {props.note}/10
+          </Typography>
+        }
       />
-      <CardMedia
-        component="img"
-        height="200"
-        image="https://cdn.pocket-lint.com/r/s/970x/assets/images/140219-tv-news-feature-netflix-tips-and-tricks-how-to-master-your-binge-watching-experience-image4-rivlhfwq6o-jpg.webp?v1"
-      />
+      <CardMedia component="img" height="600rem" image={props.img} />
       <CardContent sx={{ backgroundColor: "#F6F7EB", color: "#262730" }}>
-        <Typography variant="body2">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-          non quod voluptate hic expedita repudiandae est quasi magni sunt
-          asperiores. Adipisci alias sequi vel saepe quo eos reiciendis!
-          Repellendus, molestiae.
-        </Typography>
+        <Typography variant="body2">{props.synopsis}</Typography>
       </CardContent>
     </Card>
   );
